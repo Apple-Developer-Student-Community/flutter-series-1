ef2a51734d3be34da2d970b54d72fb3d49269301
tanishksahni <tanishk.sahni2002@gmail.com>
2023-04-27 00:45:27 +0530

    Splash screen done

--- lib/Screens/SplashScreen/splash.dart
+++ lib/Screens/SplashScreen/splash.dart
@@ -1,4 +1,9 @@
+import 'package:community_app/Screens/HomeScreen/home.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../../main.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -7,8 +12,32 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MyHomeScreen(),
+          ));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/apple.png'),
+          const SpinKitChasingDots(
+            color: Color.fromARGB(255, 252, 254, 255),
+            size: 50.0,
+          )
+        ],
+      )),
+    );
   }
 }
